1998cf291c56d1fa5ce97edce1911c6d65b4099e
fsainz <fsainz@gmail.com>
2014-09-11 17:13:41 +0200

    Devise bootstrap haml views

--- app/helpers/error_messages_helper.rb
+++ app/helpers/error_messages_helper.rb
@@ -12,4 +12,24 @@ module ErrorMessagesHelper
     str
   end
 
+  # taken from https://github.com/hisea/devise-bootstrap-views/blob/master/lib/devise_bootstrap_views_helper.rb
+  def bootstrap_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+      count: resource.errors.count,
+      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-block">
+      <button type="button" class="close" data-dismiss="alert">x</button>
+      <h5>#{sentence}</h4>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
