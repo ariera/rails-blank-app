c734914344b265896efa08562eedfb5232d87275
Alejandro Riera <ariera@gmail.com>
2014-09-11 17:21:49 +0200

    adding seo_vars and return_here_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,25 @@ class ApplicationController < ActionController::Base
   def current_user
     User.first
   end
+  helper_method :seo_vars, :return_here_url
+
+  private
+
+  def return_here_url(query={})
+    _return = Addressable::URI.parse(request.fullpath)
+    _return.query_values = (HashWithIndifferentAccess.new(_return.query_values) || {}).merge(query)
+    _return.scheme= request.scheme
+    _return.host = request.host
+    # _return.port = CONFIG[:"#{request.scheme}_port"]
+    _return.to_s
+  end
+
+  def seo_vars
+    @seo_vars ||= {
+      :title => t('seo_default_title'),
+      :meta_desc => t('seo_default_desc'),
+      :meta_lang => I18n.locale
+    }
+  end
+
 end
--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -1,6 +1,7 @@
 class DevController < ApplicationController
   layout "public"
   before_filter :must_be_in_development!
+  before_filter :set_seo_vars
 
   if Rails.env.development? || Rails.env.test? || Rails.env.staging?
     def impersonate
@@ -15,5 +16,13 @@ class DevController < ApplicationController
 
   def must_be_in_development!
     raise ActiveRecord::RecordNotFound unless Rails.env.development? || Rails.env.test?
+  end
+
+  def set_seo_vars(node=nil)
+    case self.action_name.to_sym
+    when :locales
+      seo_vars[:title] = t('dev.locales_seo_title')
+      seo_vars[:desc]  = t('dev.locales_intro')
+    end
   end
 end
