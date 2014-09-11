185f8a832badd19eb6d2e970cbb7e55558ab6be0
fsainz <fsainz@gmail.com>
2014-09-11 15:33:50 +0200

    layout public as default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout "public"
+
   include LocaleAware
   before_filter :set_locale
   before_filter :set_user_locale_if_unset
