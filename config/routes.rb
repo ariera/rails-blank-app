356ffeb2c75bc54d2adcb29f61f588dd6b536df2
fsainz <fsainz@gmail.com>
2014-09-15 09:38:00 +0200

    Root to dashboard when logged in
    all tests green

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require File.expand_path("../../lib/logged_in_constraint", __FILE__)
+
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => {
@@ -8,8 +10,9 @@ Rails.application.routes.draw do
     :unlocks       => "users/unlocks",
   }
 
-  root 'static#landing'
   match "/page/:action" => "static#:action", :via => :all, :as => 'page'
   match "/dev/:action" => "dev#:action", :via => :all, :as => 'dev'
 
+  get '/' => "static#landing",           :constraints => LoggedInConstraint::User.new(false), :as => "root"
+  get '/' => "static#dashboard_example", :constraints => LoggedInConstraint::User.new(true),  :as => "dashboard_example"
 end
