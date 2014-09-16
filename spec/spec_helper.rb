33e4c9b25c32df84574c0a41b0c1cd40e3987f71
fsainz <fsainz@gmail.com>
2014-09-16 17:11:36 +0200

    rspec devise fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include AppMacros
+  config.include Devise::TestHelpers, :type => :controller
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
