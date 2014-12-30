9fe293914118897827c5349ec2d2a8a1c813a28e
Alejandro Riera <ariera@gmail.com>
2014-12-30 10:24:47 +0100

    adding i18n fallbacks

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,10 @@ module App
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :es]
+    config.i18n.fallbacks = {
+      'en'    => ['es'],
+      'es'    => ['en'],
+    }
     config.i18n.default_locale = :en
 
     config.generators do |g|
